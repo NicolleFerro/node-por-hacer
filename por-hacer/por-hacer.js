527d34a0e4210c5eba0c4763f21ce961f75191e7
Nicolle <nicolle.ferro@gmail.com>
2020-05-20 20:10:15 -0500

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+
+//arreglo me permitira mostrar listado y manejar las tareas de manera independiente
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err)
+            throw new Error('No se pudo grabar', err);
+    });
+
+}
+
+const cargarDB = () => {
+
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+
+}
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    }
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+
+    return porHacer;
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    listadoPorHacer[index].completado = completado;
+
+    if (index >= 0) {
+        guardarDB();
+        return true;
+    } else
+        return false;
+
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+
+    let nuevoListado = listadoPorHacer.filter(tarea => tarea.descripcion !== descripcion);
+
+    if (listadoPorHacer.length === nuevoListado.length)
+        return false;
+    else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+    //mi solucion
+    // let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    // if (index >= 0) {
+    //     listadoPorHacer.splice(index, 1);
+    //     guardarDB();
+    //     return true;
+    // } else
+    //     return false;
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
